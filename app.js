84d4513fadc6fc3baaa8e6fbd003d1f3a22c2726
Sudhanshu Singh <sudsingh32@gmail.com>
2023-01-28 03:53:53 +0530

    Template and layout in EJS is used.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser=require("body-parser");
 
 const app=express();
 let items=[];
+let workItems=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -15,12 +16,23 @@ app.get("/",function(req,res) {
         day:"numeric",
     };
     let day=today.toLocaleDateString("en-US",options);
-    res.render("lists",{kindOfDay:day,newListItems:items});
+    res.render("lists",{listTitle:day,newListItems:items});
 });
 app.post("/",function(req,res){
-   let item=req.body.newItem;
-   items.push(item);
-    res.redirect("/");
+    let item=req.body.newItem;
+    if(req.body.list==="Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+        items.push(item);
+        res.redirect("/");
+    }
+   
+
+});
+app.get("/work",function(req,res){
+    res.render("lists",{listTitle:"Work",newListItems:workItems})
 });
 app.listen(3000,function(){
     console.log("Server started on 3000 port");
