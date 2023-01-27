0aeb171593d105ed693747da2c1a734da2c15e33
Sudhanshu Singh <sudsingh32@gmail.com>
2023-01-28 00:30:55 +0530

    What day is today using ejs(Embedded JavaScript).

---
+++ app.js
@@ -0,0 +1,41 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+
+const app=express();
+app.set('view engine', 'ejs');
+
+app.get("/",function(req,res) {
+
+    let today=new Date();
+    let day="";
+    let currentDay=today.getDay();
+    switch (currentDay) {
+            case 0:
+            day="Sunday";
+            break;
+            case 1:
+            day="Monday";
+            break;
+            case 2:
+            day="Tuesday";
+            break;
+            case 3:
+            day="Wednesday";
+            break;
+            case 4:
+            day="Thursday";
+            break;
+            case 5:
+            day="Friday";
+            break;
+            case 6:
+            day="Saturday";
+            break;
+        default:
+            console.log("Error:Current day is equal to: " + currentDay);
+    }
+    res.render("lists",{kindOfDay:day});
+});
+app.listen(3000,function(){
+    console.log("Server started on 3000 port");
+})
