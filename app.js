f7611f8a6ac5f3c811732140cc9e4e713dd75637
Sudhanshu Singh <sudsingh32@gmail.com>
2023-01-28 03:16:54 +0530

    The app is created after adding css and making interface presentable.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app=express();
 let items=[];
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.get("/",function(req,res) {
 
     let today=new Date();
