732945b51e5177157c0934f1be3b97281b521aa4
Sudhanshu Singh <sudsingh32@gmail.com>
2023-01-28 02:46:16 +0530

    todo List skeleton added.

--- app.js
+++ app.js
@@ -2,39 +2,24 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const app=express();
+let items=[];
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res) {
 
     let today=new Date();
-    let day="";
-    let currentDay=today.getDay();
-    switch (currentDay) {
-            case 0:
-            day="Sunday";
-            break;
-            case 1:
-            day="Monday";
-            break;
-            case 2:
-            day="Tuesday";
-            break;
-            case 3:
-            day="Wednesday";
-            break;
-            case 4:
-            day="Thursday";
-            break;
-            case 5:
-            day="Friday";
-            break;
-            case 6:
-            day="Saturday";
-            break;
-        default:
-            console.log("Error:Current day is equal to: " + currentDay);
-    }
-    res.render("lists",{kindOfDay:day});
+    let options={
+        weekday:"long",
+        month:"long",
+        day:"numeric",
+    };
+    let day=today.toLocaleDateString("en-US",options);
+    res.render("lists",{kindOfDay:day,newListItems:items});
+});
+app.post("/",function(req,res){
+   let item=req.body.newItem;
+   items.push(item);
+    res.redirect("/");
 });
 app.listen(3000,function(){
     console.log("Server started on 3000 port");
